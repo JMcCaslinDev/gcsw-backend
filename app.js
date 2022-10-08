1d6472c990d3dcc34a3c41255b3da7c58ce28039
Pedro Gutierrez Jr <gutierrezpedro2112@gmail.com>
2022-10-08 12:56:52 -0700

    added basic authentication to api endpoints

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 require('dotenv').config();
+const { auth } = require('express-openid-connect'); 
 
 const app = express();
 const port = process.env.PORT || 2000;
 
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+    baseURL: process.env.BASE_URL,
+    clientID: process.env.CLIENT_ID,
+    secret: process.env.SECRET
+}
+
 app.use(cors());
 app.use(express.json());
+app.use(auth(config));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true } 
@@ -20,6 +31,7 @@ connection.once('open', () => {
 });
 
 const usersRouter = require('./routes/users');
+const { propfind } = require("./routes/users");
 
 app.use('/users', usersRouter);
 
@@ -30,7 +42,7 @@ app.use('/users', usersRouter);
  * Temporary 'hello world' for testing
  */
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
 app.listen(port, () => {
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,16 @@
 const router = require('express').Router();
+const { requiresAuth } = require('express-openid-connect');
 let User = require('../models/user.model');
 
 // default route, gets all users in the db
-router.route('/').get((req, res) => {
+router.route('/').get(requiresAuth(), (req, res) => {
     User.find()
         .then(users => res.json(users))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
 // adds a user to the db
-router.route('/add').post((req, res) => {
+router.route('/add').post(requiresAuth(), (req, res) => {
     const username = req.body.username;
 
     // create a new user
