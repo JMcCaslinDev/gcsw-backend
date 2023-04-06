b8d742589f1bde162d34a7c355ce8d6dff270391
Ulysses Zamora <leezam01@gmail.com>
2023-04-06 11:08:20 -0700

    api admin logout

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 6060;
 
 app.use(cors());
 app.use(express.json());
